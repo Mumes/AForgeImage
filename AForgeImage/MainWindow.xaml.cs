75405627f957af31f283db23b9fcaab62a53cd79
Mumes <rodionov.max88@gmail.com>
2019-04-19 00:00:23 +0300

    firsr and probably last commit

---
+++ AForgeImage/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Drawing;
+using GonImageProcessor;
+namespace AForgeImage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png*|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                BitmapImage mapimg;
+                Bitmap img;
+                mapimg= new BitmapImage(new Uri(op.FileName));
+                imgPhoto.Source = mapimg;
+                img = new Bitmap(op.FileName);
+                ImgProcessor gon = new ImgProcessor(img);
+                gon.calcCross();
+                textBoxX.Text = $"Cмещение X {gon.X.ToString()}";
+                textBoxY.Text = $"Смещение Y {gon.Y.ToString()}";
+            }
+        }
+    }
+}
---
+++ GonImageProcessor/Processor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AForge.Imaging;
+using System.Drawing.Imaging;
+using System.Drawing;
+
+namespace GonImageProcessor
+{
+    public class ImgProcessor
+    {
+        float x;
+        float y;
+        Bitmap Img { get; set; }
+        public float X { get; private set; }
+        public float Y { get; private set; }
+        public  ImgProcessor(Bitmap _img)
+        {
+            Img = _img;
+        }
+        public void calcCross()
+        {
+            FindBlobs();
+           X= Img.Width / 2 - x;
+           Y= Img.Height / 2 - y;
+        }
+        public void FindBlobs()
+        {
+            BlobCounterBase bc = new BlobCounter();
+            bc.FilterBlobs = true;
+            bc.MinWidth = 5;
+            bc.MinHeight = 5;
+            // process binary image
+            bc.ProcessImage(Img);
+            Blob[] blobs = bc.GetObjects(Img, false);
+            x=blobs[0].CenterOfGravity.X;
+            y = blobs[0].CenterOfGravity.Y;
+        }
+    }
+}
